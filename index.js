01a96c297571ec1590c6c70a65b29859bfc8b1ce
hp <you@example.com>
2023-09-21 17:37:40 +0530

    aadd

--- index.js
+++ index.js
@@ -9,13 +9,19 @@ const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
 const cors = require('cors');
+const bodyParser = require('body-parser')
 dotenv.config();
 
 const PORT = 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.use("/images", express.static(path.join(__dirname, "/images")));
+
 
 
 mongoose
